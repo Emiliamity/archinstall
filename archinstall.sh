41bd968a20760bf7a2882ab43951259a5284646b
Emilia <93270480+Emiliamity@users.noreply.github.com>
2021-11-02 14:46:40 +0200

    Add files via upload

--- archinstall.sh
+++ archinstall.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
+fdisk /dev/sda
+n
+p
+1
+
++256M
+n
+p
+2
+
++1G
+n
+p
+3
+
+
+a
+1
+w
+q
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
 mkfs.ext4 /dev/sda3 
 mount /dev/sda3 /mnt
-pacstrap /mnt base base-devel linux linux-firmware networkmanager vim git sudo grub efibootmgr dosfstools os-prober mtools xorg xorg-server xfce4 xfce4-goodies
+pacstrap /mnt base base-devel linux linux-firmware networkmanager vim git sudo grub efibootmgr dosfstools os-prober mtools xorg xorg-server xfce4 xfce4-goodies lightdm lightdm-gtk-greeter firefox engrampa neofetch
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
@@ -24,5 +44,5 @@ mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 mkinitcpio -P
-exit
-umount -l /mnt
+systemctl enable lightdm
+neofetch
