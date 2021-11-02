7cdd0048a3eec41b3630c48d2dcdbd72ad23c422
Emilia <93270480+Emiliamity@users.noreply.github.com>
2021-11-02 16:13:33 +0200

    Add files via upload

--- archinstall.sh
+++ archinstall.sh
@@ -36,13 +36,17 @@ echo 127.0.0.1	localhost >> /etc/hosts
 echo ::1	localhost >> /etc/hosts
 echo 127.0.1.1	Arch.localdomain	Arch
 useradd -m -G wheel amity
+echo Enter the root account password
 passwd
+echo Enter the user account password
 passwd amity
+mkinitcpio -P
 systemctl enable NetworkManager
+systemctl enable lightdm
 mkdir /boot/EFI
 mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
-mkinitcpio -P
-systemctl enable lightdm
 neofetch
+exit
+umount -l /mnt
